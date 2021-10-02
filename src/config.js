dd6a17f4772097ba22d27b2a777a023350b36e84
VladYoSlav <vaifaer@gmail.com>
2021-10-03 02:20:00 +0300

    Renamed environment variables

--- src/config.js
+++ src/config.js
@@ -3,10 +3,10 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const token = process.env.TOKEN
-const userToken = process.env.USERTOKEN
-const groupId = process.env.GROUPID
+const userToken = process.env.USER_TOKEN
+const groupId = process.env.GROUP_ID
 const admins = process.env.ADMINS.split(' ').map(admin => +admin)
-const postgreUri = process.env.POSTGREURI
+const postgreUri = process.env.POSTGRE_URI
 const isDev = process.env.NODE_ENV === 'development'
 
 export {
